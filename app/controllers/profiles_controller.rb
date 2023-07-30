5b49e1510362f7aca487a0d72e60e8b1fd96fc6d
magusch <magusch@gmail.com>
2023-07-30 22:31:24 +0200

    some design improving for profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,19 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  def index
+    @users = User.all
+  end
   def show
-    @user = current_user # Assuming you are using Devise for authentication
+    @user =
+      if params[:id]== "me"
+        if current_user
+          current_user
+        else
+          flash[:success] = "You are not authorized to view this page"
+          redirect_to new_user_session_path
+        end
+      else
+        User.find(params[:id])
+      end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
   post 'articles/:article_id/comments/:comment_id/vote', to: 'comment_votes#create', as: :upvote_article_comment
   delete 'articles/:article_id/comments/:comment_id/vote', to: 'comment_votes#destroy', as: :unvote_article_comment
 
-  get '/profile', to: 'profiles#show', as: :user_profile
+  get '/profile/:id', to: 'profiles#show', as: :profile
+  get '/profile/me', to: 'profiles#show', as: :user_profile
+  get '/profiles', to: 'profiles#index', as: :profiles
 end
