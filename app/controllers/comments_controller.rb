218313438b4ca40785729a558f8c30092f78edce
magusch <magusch@gmail.com>
2023-08-07 17:58:50 +0200

    Comments: web design improving

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,11 +19,26 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
+    @comment = Comment.find(params[:id])
+    @article = @comment.article
     @comment.destroy
     flash[:success] = "Comment was deleted"
     redirect_to article_path(@article), status: :see_other
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @article = @comment.article
+    if @comment.update(comment_params)
+      flash[:success] = "Comment was successfully updated"
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
