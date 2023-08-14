cb8f3f0497c602d5b71967846ea3e858a832a968
magusch <magusch@gmail.com>
2023-08-14 14:37:18 +0200

    Comments: try turbo_stream (don't work now)

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module BlogMagNew
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.generators.template_engine = :turbo_stream
 
     # Configuration for the application, engines, and railties goes here.
     #
