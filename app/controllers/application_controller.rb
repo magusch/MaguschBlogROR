c9be9c5ae780300ebc28e311ae90c832168d60fe
magusch <magusch@gmail.com>
2023-08-14 14:39:27 +0200

    Footer options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def set_pages
-    @pages_navbar = Page.where(navbar_display: true)
+    @pages_navbar = Page.where(navbar_display: true).limit(5)
+    @pages_footer = Page.where(navbar_display: true).limit(3)
   end
 end
