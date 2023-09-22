8929a1a15482add8458c8f1d9837f3aa01d694bc
magusch <magusch@gmail.com>
2023-09-22 17:12:54 +0200

    v0.2.1: basic html editing

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,10 +3,10 @@ module ArticlesHelper
   def prepare_article(markdown_text, images=nil)
     images = [] if images.nil?
     images.each_with_index do |image, i|
-      # html_answer.gsub!(/#{image[:name]}/, image_tag(image[:url]))
-
       markdown_text.gsub!("<image-#{i+1}>", image)
     end
+
+    markdown_text.gsub!(/<image-\d+>/, '')
 
     markdown(markdown_text)
   end
